95de54678605579425d13a35e7cdb53917c146dc
Knox <yzhaodx@connect.ust.hk>
2025-01-30 17:50:27 +0800

    New version

--- README.md
+++ README.md
@@ -1,20 +1,54 @@
-# vCard - Personal portfolio
+<div align="center">
+  
+  ![GitHub repo size](https://img.shields.io/github/repo-size/codewithsadee/richard-ryan)
+  ![GitHub stars](https://img.shields.io/github/stars/codewithsadee/richard-ryan?style=social)
+  ![GitHub forks](https://img.shields.io/github/forks/codewithsadee/richard-ryan?style=social)
+  [![Twitter Follow](https://img.shields.io/twitter/follow/codewithsadee?style=social)](https://twitter.com/intent/follow?screen_name=codewithsadee)
+  [![YouTube Video Views](https://img.shields.io/youtube/views/jOA6ROBXdRE?style=social)](https://youtu.be/jOA6ROBXdRE)
 
-![GitHub repo size](https://img.shields.io/github/repo-size/KNOXZhao/knoxzhao.github.io)
+  <br />
+  <br />
 
-vCard is a fully responsive personal portfolio website, responsive for all devices, built using HTML, CSS, and JavaScript.
+  <h2 align="center">Richard Ryan - Photographer Portfolio Website</h2>
 
+  Richard Ryan is a fully responsive portfolio website, <br />Responsive for all devices, build using HTML, CSS, and JavaScript.
 
-## Prerequisites
+  <a href="https://codewithsadee.github.io/richard-ryan/"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Richard Ryan Desktop Demo](./readme-images/desktop.png "Desktop Demo")
+
+### Prerequisites
 
 Before you begin, ensure you have met the following requirements:
 
 * [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
 
-## Contact
+### Run Locally
 
-If you want to contact me you can reach me at [Email](yzhaodx@connect.ust.hk).
+To run **Richard Ryan** locally, run this command on your git bash:
 
-## License
+Linux and macOS:
 
-MIT license.
+```bash
+sudo git clone https://github.com/codewithsadee/richard-ryan.git
+```
+
+Windows:
+
+```bash
+git clone https://github.com/codewithsadee/richard-ryan.git
+```
+
+### Contact
+
+If you want to contact with me you can reach me at [Twitter](https://www.twitter.com/codewithsadee).
+
+### License
+
+This project is **free to use** and does not contains any license.
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,158 +2,237 @@
 
 
 
-// element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+// add Event on multiple elment
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
 
 
 
-// sidebar variables
-const sidebar = document.querySelector("[data-sidebar]");
-const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+// PRELOADING
 
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", function () {
+  loadingElement.classList.add("loaded");
+  document.body.classList.remove("active");
+});
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
+// MOBILE NAV TOGGLE
 
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll("[data-nav-toggler]"),
+  document.querySelectorAll("[data-nav-link]"),
+  document.querySelector("[data-navbar]"),
+  document.querySelector("[data-overlay]")
+];
 
-// modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
+const toggleNav = function () {
+  navbar.classList.toggle("active");
   overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
 }
 
-// add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+addEventOnElements(navTogglers, "click", toggleNav);
 
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
+const closeNav = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("active");
 }
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+addEventOnElements(navLinks, "click", closeNav);
 
 
 
-// custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
+// HEADER
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+const header = document.querySelector("[data-header]");
 
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
+const activeElementOnScroll = function () {
+  if (window.scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 }
 
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
+window.addEventListener("scroll", activeElementOnScroll);
 
-const filterFunc = function (selectedValue) {
 
-  for (let i = 0; i < filterItems.length; i++) {
 
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
+/**
+ * TEXT ANIMATION EFFECT FOR HERO SECTION
+ */
+
+const letterBoxes = document.querySelectorAll("[data-letter-effect]");
+
+let activeLetterBoxIndex = 0;
+let lastActiveLetterBoxIndex = 0;
+let totalLetterBoxDelay = 0;
+
+const setLetterEffect = function () {
+
+  // loop through all letter boxes
+  for (let i = 0; i < letterBoxes.length; i++) {
+    // set initial animation delay
+    let letterAnimationDelay = 0;
+
+    // get all character from the current letter box
+    const letters = letterBoxes[i].textContent.trim();
+    // remove all character from the current letter box
+    letterBoxes[i].textContent = "";
+
+    // loop through all letters
+    for (let j = 0; j < letters.length; j++) {
+
+      // create a span
+      const span = document.createElement("span");
+
+      // set animation delay on span
+      span.style.animationDelay = `${letterAnimationDelay}s`;
+
+      // set the "in" class on the span, if current letter box is active
+      // otherwise class is "out"
+      if (i === activeLetterBoxIndex) {
+        span.classList.add("in");
+      } else {
+        span.classList.add("out");
+      }
+
+      // pass current letter into span
+      span.textContent = letters[j];
+
+      // add space class on span, when current letter contain space
+      if (letters[j] === " ") span.classList.add("space");
+
+      // pass the span on current letter box
+      letterBoxes[i].appendChild(span);
+
+      // skip letterAnimationDelay when loop is in the last index
+      if (j >= letters.length - 1) break;
+      // otherwise update
+      letterAnimationDelay += 0.05;
+
+    }
+
+    // get total delay of active letter box
+    if (i === activeLetterBoxIndex) {
+      totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
+    }
+
+    // add active class on last active letter box
+    if (i === lastActiveLetterBoxIndex) {
+      letterBoxes[i].classList.add("active");
     } else {
-      filterItems[i].classList.remove("active");
+      letterBoxes[i].classList.remove("active");
     }
 
   }
 
-}
+  setTimeout(function () {
+    lastActiveLetterBoxIndex = activeLetterBoxIndex;
 
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
+    // update activeLetterBoxIndex based on total letter boxes
+    activeLetterBoxIndex >= letterBoxes.length - 1 ? activeLetterBoxIndex = 0 : activeLetterBoxIndex++;
 
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
+    setLetterEffect();
+  }, (totalLetterBoxDelay * 1000) + 3000);
 
 }
 
+// call the letter effect function after window loaded
+window.addEventListener("load", setLetterEffect);
 
 
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
+/**
+ * BACK TO TOP BUTTON
+ */
 
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+  // visible back top btn when scrolled 5% of the page
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
+
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    const elementIsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+
+    if (elementIsInScreen) {
+      revealElements[i].classList.add("revealed");
     } else {
-      formBtn.setAttribute("disabled", "");
+      revealElements[i].classList.remove("revealed");
     }
-
-  });
+  }
 }
 
+window.addEventListener("scroll", scrollReveal);
+
+scrollReveal();
 
 
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+/**
+ * CUSTOM CURSOR
+ */
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
+const cursor = document.querySelector("[data-cursor]");
+const anchorElements = document.querySelectorAll("a");
+const buttons = document.querySelectorAll("button");
 
-  });
-}
+// change cursorElement position based on cursor move
+document.body.addEventListener("mousemove", function (event) {
+  setTimeout(function () {
+    cursor.style.top = `${event.clientY}px`;
+    cursor.style.left = `${event.clientX}px`;
+  }, 100);
+});
+
+// add cursor hoverd class
+const hoverActive = function () { cursor.classList.add("hovered"); }
+
+// remove cursor hovered class
+const hoverDeactive = function () { cursor.classList.remove("hovered"); }
+
+// add hover effect on cursor, when hover on any button or hyperlink
+addEventOnElements(anchorElements, "mouseover", hoverActive);
+addEventOnElements(anchorElements, "mouseout", hoverDeactive);
+addEventOnElements(buttons, "mouseover", hoverActive);
+addEventOnElements(buttons, "mouseout", hoverDeactive);
+
+// add disabled class on cursorElement, when mouse out of body
+document.body.addEventListener("mouseout", function () {
+  cursor.classList.add("disabled");
+});
+
+// remove diabled class on cursorElement, when mouse in the body
+document.body.addEventListener("mouseover", function () {
+  cursor.classList.remove("disabled");
+});
